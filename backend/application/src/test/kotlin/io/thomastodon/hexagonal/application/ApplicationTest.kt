bfcc953fd5b179a8f0b42ae7940f1cf5f6b4561b
Thomas Shouler <thomas@>
2018-07-15 20:32:05 -0500

    Seed application/acceptance tests

---
+++ backend/application/src/test/kotlin/io/thomastodon/hexagonal/application/ApplicationTest.kt
@@ -0,0 +1,16 @@
+package io.thomastodon.hexagonal.application
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@SpringBootTest
+@ExtendWith(SpringExtension::class)
+class ApplicationTest {
+
+
+    @Test
+    fun `test it all`() {
+    }
+}
